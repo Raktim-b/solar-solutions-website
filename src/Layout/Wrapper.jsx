ca6abdd184d3d0601d6b68fe04d9dac706a3d990
developeravijit <roysidharth1996@gmail.com>
2026-03-11 02:20:20 +0530

    added gsap

--- src/Layout/Wrapper.jsx
+++ src/Layout/Wrapper.jsx
@@ -22,25 +22,45 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Header/Navbar";
 import Footer from "../Components/Footer/Footer";
 import Loader from "../Components/Loader/Loader";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const Wrapper = () => {
   const [loading, setLoading] = useState(true);
+  const pageRef = useRef(null);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1200); // match loader animation duration
+    }, 1200);
 
     return () => clearTimeout(timer);
   }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      gsap.set(pageRef.current, {
+        opacity: 0,
+        scale: 1.3,
+        y: 40,
+      });
+
+      gsap.to(pageRef.current, {
+        opacity: 1,
+        scale: 1,
+        y: 0,
+        duration: 1,
+        ease: "power3.out",
+      });
+    }
+  }, [loading]);
 
   return (
     <>
       {loading && <Loader />}
 
       {!loading && (
-        <main>
+        <main ref={pageRef}>
           <Navbar />
           <Outlet />
           <Footer />
